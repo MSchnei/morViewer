1fd1958a5e780fb948116fb584bee0698fd5f8db
MSchnei <marian.schneider@maastrichtuniversity.nl>
2018-02-08 16:51:14 +0100

    updated naming

--- morph_viewer.py
+++ morph_viewer.py
@@ -9,14 +9,19 @@ Created on Fri Apr 21 11:48:19 2017
 import os
 import numpy as np
 from nibabel import load, save, Nifti1Image
-from scipy.ndimage import morphology, generate_binary_structure
 from pyqtgraph.Qt import QtGui
+from button_functions_functions import (updateOpen, updateClose, updateSave,
+                                        setEverySecVoxel, undoEverySecVoxel)
 import pyqtgraph as pg
 
-# provide string to nii data
-strPathData = '/Users/Marian/Documents/PrePro/PyqtTest/P01_Mask_WB.nii'
 
-# %% Create window with two ImageView widgets
+# %% set parameters
+
+# provide string to nii data
+strPathData = '/home/marian/Documents/Testing/testMorphViewer/testIma.nii.gz'
+
+
+# %% create window with two ImageView widgets
 
 # Always start by initializing Qt (only once per application)
 app = QtGui.QApplication([])
@@ -34,7 +39,8 @@ win.setCentralWidget(cw)
 l = QtGui.QGridLayout()
 cw.setLayout(l)
 
-# %%
+
+# %% import data
 
 # load data
 nii = load(strPathData)
@@ -45,65 +51,35 @@ data = nii.get_data()
 # show smallest axis first
 data = np.transpose(data, np.argsort(data.shape))
 
+# %% arrange graph
+
 # define image of the nii data with pyqtgraph
 imv1 = pg.image(data)
-
-# %% define functions for updating
-def updateOpen():
-    global data, imv1
-    # morphology structure
-    struct = generate_binary_structure(3, 1)
-    # perform opening
-    data = morphology.binary_erosion(data, structure=struct, iterations=1)
-    data = morphology.binary_dilation(data, structure=struct, iterations=1)
-    imv1.setImage(data)
-
-def updateClose():
-    global data, imv1
-    # morphology structure
-    struct = generate_binary_structure(3, 1)
-    # perform closing
-    data = morphology.binary_dilation(data, structure=struct, iterations=1)
-    data = morphology.binary_erosion(data, structure=struct, iterations=1)
-    imv1.setImage(data)
-
-def updateBorder():
-    global data, imv1
-    # morphology structure
-    struct = generate_binary_structure(3, 1)
-    # find borders
-    temp = morphology.binary_erosion(data, structure=struct, iterations=1)
-    data = data*(1-temp)
-
-def updateSave():
-    # save as nifti
-    out = Nifti1Image(data, header=nii.header, affine=nii.affine)
-    save(out, basename + "_morph.nii.gz")
-    print "data saved as: " + basename + "_morph.nii.gz"
-    
-
 # add the image widget to the layout in its proper positions
 l.addWidget(imv1, 0, 0)
 
 # create a push button widget
 btn1 = QtGui.QPushButton('Open')
 btn2 = QtGui.QPushButton('Close')
-btn3 = QtGui.QPushButton('Border')
-btn4 = QtGui.QPushButton('Save')
+btn3 = QtGui.QPushButton('DiverseVoxels')
+btn4 = QtGui.QPushButton('BinarizeVoxels')
+btn5 = QtGui.QPushButton('Save')
 
 # Add button widgets to the layout in their proper positions
 l.addWidget(btn1, 1, 0)
 l.addWidget(btn2, 2, 0)
 l.addWidget(btn3, 3, 0)
 l.addWidget(btn4, 4, 0)
+l.addWidget(btn5, 5, 0)
 
 # define what the buttons should do when clicked
 # check for more options:
 # https://www.tutorialspoint.com/pyqt/pyqt_qpushbutton_widget.htm
 btn1.clicked.connect(updateOpen)
 btn2.clicked.connect(updateClose)
-btn3.clicked.connect(updateBorder)
-btn4.clicked.connect(updateSave)
+btn3.clicked.connect(setEverySecVoxel)
+btn4.clicked.connect(undoEverySecVoxel)
+btn5.clicked.connect(updateSave)
 
 
 # %%
