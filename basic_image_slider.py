2e82df639e2658f3cd79ae803293e19e353d48f7
MSchnei <marian.schneider@maastrichtuniversity.nl>
2018-02-08 20:19:01 +0100

    added basic image slider

---
+++ basic_image_slider.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  8 19:49:03 2018
+
+@author: marian
+"""
+
+import os
+from nibabel import load, save, Nifti1Image
+from PyQt4 import QtCore, QtGui
+
+# %% set parameters
+
+# provide string to nii data
+strPathData = '/home/marian/Documents/Testing/testMorphViewer/testIma.nii.gz'
+
+# %% import data
+
+# load data
+nii = load(strPathData)
+basename = nii.get_filename().split(os.extsep, 1)[0]
+dirname = os.path.dirname(nii.get_filename())
+
+data = nii.get_data()
+
+# %% define widget
+
+class Widget(QtGui.QWidget):
+
+    def __init__(self, parent=None):
+        super(Widget, self).__init__(parent)
+        self.resize(640, 480)
+        self.layout = QtGui.QVBoxLayout(self)
+
+        self.scene = QtGui.QGraphicsScene(self)
+        self.view = QtGui.QGraphicsView(self.scene)
+        self.layout.addWidget(self.view)
+
+        self.image = QtGui.QGraphicsPixmapItem()
+        self.scene.addItem(self.image)
+        self.view.centerOn(self.image)
+
+        self._images = data
+
+        self.slider = QtGui.QSlider(self)
+        self.slider.setOrientation(QtCore.Qt.Horizontal)
+        self.slider.setMinimum(0)
+        # max is the last index of the image list
+        self.slider.setMaximum(len(self._images)-1)
+        self.layout.addWidget(self.slider)
+
+        # set it to the first image, if you want.
+        self.sliderMoved(0)
+
+        self.slider.sliderMoved.connect(self.sliderMoved)
+
+    def sliderMoved(self, val):
+        print "Slider moved to:", val
+        try:
+            self.image.setPixmap(self._images[val])
+        except IndexError:
+            print "Error: No image at index", val
+
+# %% render
+app = QtGui.QApplication([])
+w = Widget()
+w.show()
+w.raise_()
+app.exec_()
